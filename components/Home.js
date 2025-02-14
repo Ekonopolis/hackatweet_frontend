4625c79311e32e346951cf883deeaf253f1d853f
Michaël <michabarba28@gmail.com>
2025-02-14 16:41:33 +0100

    page/home

--- components/Home.js
+++ components/Home.js
@@ -1,18 +1,76 @@
 import styles from '../styles/Home.module.css';
 import Image from 'next/image';
-import React, { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { DownloadOutlined } from '@ant-design/icons';
-import { Button, Divider, Flex, Radio, AutoComplete, Space } from 'antd';
-
-
-
-
+import { Button, Divider, Flex, Radio, AutoComplete } from 'antd';
+import { useRouter } from "next/router" //!!! permet de créer une route vers une "nouvelle page url" en lien avec la ligne 23 et 54
 
 
 function Home() {
-  const handleClick = () => {
-    console.log('Click detected')
+  // Etat SignIn
+  const [usernameSignIn, setUsernameSignIn] = useState("")
+  const [passwordSignIn, setpasswordSignIn] = useState("")
+
+  // Etat SignUp
+  const [firstnameSignUp, setFirstnameSignUp] = useState("")
+  const [usernameSignUp, setUsernameSignUp] = useState("")
+  const [passwordSignUp, setPasswordSignUp] = useState("")
+
+  console.log(firstnameSignUp)
+  console.log(usernameSignUp)
+  console.log(passwordSignUp)
+
+  const router = useRouter()
+
+  // useEffect(() => {
+
+  // })
+
+  const handleRegister = () => {
+    fetch('http://localhost:3000/users/signup', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ firstname: firstnameSignUp, username: usernameSignUp, password: passwordSignUp }),
+    }).then(response => response.json())
+      .then(data => {
+        console.log('Réponse du serveur :', data)
+
+        // reset l'input à 0 lignes 94 95 96
+        setFirstnameSignUp('');
+        setUsernameSignUp('');
+        setPasswordSignUp('');
+      })
   };
+
+  const handleLogin = () => {
+    fetch('http://localhost:3000/users/signin', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ username: usernameSignIn, password: passwordSignIn }),
+    }).then(response => response.json())
+      .then(data => {
+        console.log('Réponse du serveur :', data)
+        router.push("/home")
+        setUsernameSignIn('');
+        setpasswordSignIn('');
+      })
+
+  }
+
+  // const handleRegister = () => {
+  //   fetch('http://localhost:3000/users/signup', {
+  //         method: 'POST',
+  //         headers: { 'Content-Type': 'application/json' },
+  //         body: JSON.stringify({ firstname: firstname, username: username, password: password }),
+  //       }).then(response => response.json())
+  //         .then(data => {
+  //           if (data.result) {
+  //             setFirstnameSignUp('');
+  //             setUsernameSignUp('');
+  //             setPasswordSignUp('');
+  //           };
+  //         })
+  // };
 
 
 
@@ -30,16 +88,17 @@ function Home() {
           <p>Join Hackatweet today.
           </p>
           <div>
-            <Button type="primary" onClick={() => handleClick()} className={styles.button}>Sign In</Button>
             <br />
-            <input type="text" placeholder="username" /> <br />
-          <input type="text" placeholder="password" />
-          <br />
-          <br />
-            <Button onClick={ () => handleClick()} className={styles.button}>Sign Up</Button> <br />
-            <input type="text" placeholder="firstname" /> <br />
-          <input type="text" placeholder="username" /> <br />
-          <input type="text" placeholder="password" />
+            <input type="text" placeholder="username" value={usernameSignIn} onChange={(e) => setUsernameSignIn(e.target.value)} /> <br />
+            <input type="text" placeholder="password" value={passwordSignIn} onChange={(e) => setpasswordSignIn(e.target.value)} /> <br />
+            <Button type="primary" onClick={() => handleLogin()} className={styles.button}>Sign In</Button>
+            <br />
+            <br />
+
+            <input type="text" placeholder="firstname" value={firstnameSignUp} onChange={(e) => setFirstnameSignUp(e.target.value)} /> <br />
+            <input type="text" placeholder="username" value={usernameSignUp} onChange={(e) => setUsernameSignUp(e.target.value)} /> <br />
+            <input type="text" placeholder="password" value={passwordSignUp} onChange={(e) => setPasswordSignUp(e.target.value)} /> <br />
+            <Button onClick={() => handleRegister()} className={styles.button}>Sign Up</Button>
           </div>
         </div>
       </main>
---
+++ pages/home.js
@@ -0,0 +1,15 @@
+import Home from '../components/Home';
+
+function HomePage() {
+
+    return
+    <container>
+        <div>
+            <div></div>
+            <div></div>
+            <div></div>
+        </div>
+    </container>
+}
+
+export default HomePage;
